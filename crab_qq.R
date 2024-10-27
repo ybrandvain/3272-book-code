c24ea096c68fc579ec8de1977e296491a5fac3f4
Yaniv Brandvain <ybrandvain@gmail.com>
2024-10-27 07:58:04 -0500

    Create crab_qq.R

---
+++ crab_qq.R
@@ -0,0 +1,39 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(broom)
+crab <- read_csv("https://whitlockschluter3e.zoology.ubc.ca/Data/chapter15/chap15q27FiddlerCrabFans.csv")
+
+
+### PLOT 1
+ggplot(crab , aes(x= bodyTemperature))+
+    geom_qq()+
+    geom_qq_line()+
+    facet_wrap(~crabType)
+    
+### PLOT 2
+ggplot(crab , aes(x= bodyTemperature))+
+    geom_qq()+
+    geom_qq_line()+
+    facet_wrap(~crabType)
+    
+### PLOT 3
+lm(bodyTemperature ~ crabType, data = crab) %>%
+    augment()%>%
+    ggplot(., aes(x= .fitted))+
+    geom_qq()+
+    geom_qq_line()+
+    facet_wrap(~crabType)
+    
+### PLOT 4
+lm(bodyTemperature ~ crabType, data = crab) %>%
+    augment()%>%
+    ggplot(., aes(x= .resid))+
+    geom_qq()+
+    geom_qq_line()+
+    facet_wrap(~crabType)
+    
+    
+        
+    
+    
