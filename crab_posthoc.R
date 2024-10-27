4972baa039d641f9d981339d5a5783d768f190e1
Yaniv Brandvain <ybrandvain@gmail.com>
2024-10-27 17:34:32 -0500

    Create crab_posthoc.R

---
+++ crab_posthoc.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(forcats)
+library(readr)
+library(ggplot2)
+crabs <- read_csv("https://whitlockschluter3e.zoology.ubc.ca/Data/chapter15/chap15q27FiddlerCrabFans.csv")
+
+crabs <- crabs %>% 
+  na.omit()%>%
+  mutate(crabType = fct_relevel(crabType,   "female",   "male major removed", "male minor removed",  "intact male"))
+
+ggplot(data= crabs, aes(x = crabType,  y = bodyTemperature, color = crabType))+
+  geom_jitter(width = .2, size = 3, alpha = .7, show.legend = FALSE)+
+  stat_summary(fun.data=mean_cl_normal, geom ="errorbar", width =.2, position = position_nudge(x=.4))+
+  geom_text(data = tibble(crabType = c("female","male minor removed"), sig_group = c("A","C")),
+              aes(y = 2.2,label = sig_group), size = 8)+
+  theme(legend.position = "none",
+        axis.title = element_text(size = 15),
+        axis.text = element_text(size = 12))+
+  labs(x = "crab type", y = "body temperature")
+
+aov(bodyTemperature ~ crabType, data = crabs) %>%
+  TukeyHSD()
